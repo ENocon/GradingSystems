df5c84f81a796a54cb65c1080690e95aa980fcd6
ENocon <elizabeth.nocon@outlook.com>
2019-01-31 19:14:38 -0700

    Initial commit practice. Only committing project description and imported libraries.

--- GradingSystem/src/Linked_List_Grading_System.java
+++ GradingSystem/src/Linked_List_Grading_System.java
@@ -1,4 +1,23 @@
+//Programmer: Elizabeth Nocon
+//Description:
+//This program will ask the user to input student names and grades. 
+//The program will use this information to create a linked list. 
+//The program will then print the student names and grades in order
+//of highest grade to lowest. The program will also create a text document. 
 
-public class Linked_List_Grading_System {
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.lang.Comparable;
 
-}
+public class Linked_List_Grading_System 
+{
+	public static void main (String [] args) throws Exception
+	{
+		
+	} //Main
+} //Linked_List_Grading_System
